5f2b29d9ed79f9c8339c8ea87909f297b30fd553
Rohit Mistry <rmist@microsoft.com>
2015-10-19 10:12:54 +0100

    Removed all jspm_packages files

--- public/jspm_packages/npm/angular2@2.0.0-alpha.44/es6/dev/src/core/zone.js
+++
@@ -1,5 +0,0 @@
-/* */ 
-"format cjs";
-// Public API for Zone
-export { NgZone } from './zone/ng_zone';
-//# sourceMappingURL=zone.js.map
--- public/jspm_packages/npm/angular2@2.0.0-alpha.44/src/core/zone.js
+++
@@ -1,4 +0,0 @@
-/* */ 
-'use strict';
-var ng_zone_1 = require('./zone/ng_zone');
-exports.NgZone = ng_zone_1.NgZone;
